6ce29e556a0d307f1c5fe2b687b6d808f9551823
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2020-07-19 18:33:52 +0300

    Add files via upload

--- src/EnemyAndPacman/Pacman.java
+++ src/EnemyAndPacman/Pacman.java
@@ -1,4 +1,337 @@
+package EnemyAndPacman;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+import Final.PacmanField;
+
 public class Pacman {
-  public static void main(String[] args) {
-  }
+
+	public static int PosHeroI;
+	public static int PosHeroJ;
+	public static int stateHero;
+	public static int Level;
+
+	public Pacman(int posHeroI, int posHeroJ, int level) {
+		PosHeroI = posHeroI;
+		PosHeroJ = posHeroJ;
+		Level = level;
+		stateHero = -1;
+
+	}
+
+	public Pacman() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void moveToLeft() {
+		HeroEatLeft();
+		if (Level == 1) {
+			if (PacmanField.array[PosHeroI][PosHeroJ - 1] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PacmanField.array[PosHeroI][PosHeroJ - 1] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroJ > 1) { // граница поля
+					PosHeroJ--;
+				}
+			}
+		}
+		if (Level == 2 || Level == 3) {
+			if ((PosHeroI == 10 && PosHeroJ == 0)
+					|| PacmanField.array[PosHeroI][PosHeroJ - 1] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PosHeroJ != 0
+						&& PacmanField.array[PosHeroI][PosHeroJ - 1] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroJ > 0) { // граница поля
+					PosHeroJ--;
+
+				} else if (PosHeroJ == 0 && PosHeroI == 10) {
+					PosHeroJ = 28;
+				}
+
+			}
+
+		}
+
+	}
+
+	public static void moveToRight() {
+		HeroEatRight();
+		if (Level == 1) {
+			if (PacmanField.array[PosHeroI][PosHeroJ + 1] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PacmanField.array[PosHeroI][PosHeroJ + 1] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroJ < PacmanField.lastJ) {
+					PosHeroJ++;
+				}
+			}
+		}
+		if (Level == 2 || Level == 3) {
+			if ((PosHeroI == 10 && PosHeroJ == 28)
+					|| PacmanField.array[PosHeroI][PosHeroJ + 1] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PosHeroJ != 28
+						&& PacmanField.array[PosHeroI][PosHeroJ + 1] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroJ < PacmanField.lastJ) {
+					PosHeroJ++;
+				} else if (PosHeroJ == 28 && PosHeroI == 10) {
+					PosHeroJ = 0;
+				}
+			}
+		}
+
+	}
+
+	public static void moveDown() {
+		HeroEatDown();
+		if (Level == 1) {
+			if (PacmanField.array[PosHeroI + 1][PosHeroJ] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PacmanField.array[PosHeroI + 1][PosHeroJ] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroI < PacmanField.lastI) {
+					PosHeroI++;
+				}
+			}
+		}
+		if (Level == 2 || Level == 3) {
+			if (PacmanField.array[PosHeroI + 1][PosHeroJ] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PacmanField.array[PosHeroI + 1][PosHeroJ] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroI < PacmanField.lastI) {
+					PosHeroI++;
+				}
+
+			}
+		}
+
+	}
+
+	public static void moveUp() {
+		HeroEatUp();
+		if (Level == 1) {
+			if (PacmanField.array[PosHeroI - 1][PosHeroJ] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PacmanField.array[PosHeroI - 1][PosHeroJ] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroI > 1) {
+					PosHeroI--;
+				}
+			}
+		}
+		if (Level == 2 || Level == 3) {
+			if (PacmanField.array[PosHeroI - 1][PosHeroJ] != 1 && checkEnemy()!=0) {
+				PacmanField.haveEat();
+				if (PacmanField.array[PosHeroI - 1][PosHeroJ] == 9) {
+					PacmanField.setEnergizer(true);
+					PacmanField.countPoint += 100;
+				}
+				PacmanField.array[PosHeroI][PosHeroJ] = 8;
+				if (PosHeroI > 1) {
+					PosHeroI--;
+				}
+			}
+		}
+	}
+
+	public static void HeroEatRight() {
+		if (stateHero == 5)
+			stateHero = 2;
+		else
+			stateHero = 5;
+	}
+
+	public static void HeroEatLeft() {
+		if (stateHero == 6)
+			stateHero = 0;
+		else
+			stateHero = 6;
+	}
+
+	public static void HeroEatUp() {
+		if (stateHero == 7)
+			stateHero = 3;
+		else
+			stateHero = 7;
+	}
+
+	public static void HeroEatDown() {
+		if (stateHero == 8)
+			stateHero = 1;
+		else
+			stateHero = 8;
+	}
+
+	public static int checkEnemy() {
+		if (PosHeroI != PacmanEnemyGreen.posEnemyI
+				&& PosHeroJ != PacmanEnemyGreen.posEnemyJ
+				|| PosHeroI != PacmanEnemyOrange.posEnemyI
+				&& PosHeroJ != PacmanEnemyOrange.posEnemyJ
+				|| PosHeroI != PacmanEnemyRed.posEnemyI
+				&& PosHeroJ != PacmanEnemyRed.posEnemyJ
+				|| PosHeroI != PacmanEnemyPink.posEnemyI
+				&& PosHeroJ != PacmanEnemyPink.posEnemyJ) {
+			return 1;
+		} else {
+			PacmanField.gameIsOver = true;
+			return 0;
+		}
+	}
+
+	public void drawHeroUp(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		Polygon j = new Polygon();
+		j.addPoint(CELL_WIDTH * PosHeroJ + 3 * size, CELL_HEIGHT * PosHeroI);
+		j.addPoint(CELL_WIDTH * PosHeroJ + 8 * size, CELL_HEIGHT * PosHeroI);
+		j.addPoint(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 6);
+		g.setColor(Color.BLACK);
+		g.fillPolygon(j);
+		g.drawPolygon(j);
+	}
+
+	public void drawHeroDown(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		Polygon j = new Polygon();
+		j.addPoint(CELL_WIDTH * PosHeroJ + 3 * size, CELL_HEIGHT * PosHeroI
+				+ size * 12 + size / 2);
+		j.addPoint(CELL_WIDTH * PosHeroJ + 8 * size, CELL_HEIGHT * PosHeroI
+				+ size * 12 + size / 2);
+		j.addPoint(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 6);
+		g.setColor(Color.BLACK);
+		g.fillPolygon(j);
+		g.drawPolygon(j);
+	}
+
+	public void drawHeroLeft(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		Polygon j = new Polygon();
+		j.addPoint(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI + size * 5);
+		j.addPoint(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI + size * 8);
+		j.addPoint(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 6);
+		g.setColor(Color.BLACK);
+		g.fillPolygon(j);
+		g.drawPolygon(j);
+	}
+
+	public void drawHeroRight(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		Polygon j = new Polygon();
+		j.addPoint(CELL_WIDTH * PosHeroJ + size * 12 + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 5);
+		j.addPoint(CELL_WIDTH * PosHeroJ + size * 12 + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 8);
+		j.addPoint(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 6);
+		g.setColor(Color.BLACK);
+		g.fillPolygon(j);
+		g.drawPolygon(j);
+	}
+
+	public void drawHeroEatRight(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawLine(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 6, CELL_WIDTH * PosHeroJ + size * 12 + size
+				/ 2, CELL_HEIGHT * PosHeroI + size * 6);
+	}
+
+	public void drawHeroEatLeft(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawLine(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI + size * 6,
+				CELL_WIDTH * PosHeroJ + size * 6 + size / 2, CELL_HEIGHT
+						* PosHeroI + size * 6);
+	}
+
+	public void drawHeroEatUp(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawLine(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI, CELL_WIDTH * PosHeroJ + 6 * size + size / 2,
+				CELL_HEIGHT * PosHeroI + size * 6 + size / 2);
+	}
+
+	public void drawHeroEatDown(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int size = 2;
+		g.fillOval(CELL_WIDTH * PosHeroJ, CELL_HEIGHT * PosHeroI, CELL_WIDTH,
+				CELL_HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawLine(CELL_WIDTH * PosHeroJ + 6 * size + size / 2, CELL_HEIGHT
+				* PosHeroI + size * 6 + size / 2, CELL_WIDTH * PosHeroJ + size
+				* 6 + size / 2, CELL_HEIGHT * PosHeroI + size * 12 + size / 2);
+	}
+
+	final public static int CELL_HEIGHT = 24;
+	final public static int CELL_WIDTH = 24;
+
+	public void drawHero(Graphics g, int width, int height) {
+		if (stateHero == 1)
+			drawHeroDown(g);
+		else if (stateHero == 0)
+			drawHeroLeft(g);
+		else if (stateHero == 2 || stateHero == -1)
+			drawHeroRight(g);
+		else if (stateHero == 3)
+			drawHeroUp(g);
+		else if (stateHero == 5)
+			drawHeroEatRight(g);
+		else if (stateHero == 6)
+			drawHeroEatLeft(g);
+		else if (stateHero == 7)
+			drawHeroEatUp(g);
+		else if (stateHero == 8)
+			drawHeroEatDown(g);
+	}
+
 }
---
+++ src/EnemyAndPacman/PacmanEnemyOrange.java
@@ -0,0 +1,272 @@
+package EnemyAndPacman;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+import Final.PacmanField;
+
+public class PacmanEnemyOrange {
+	public static int posEnemyI;
+	public static int posEnemyJ;
+
+	private static int[] mass;
+	private static int imin;
+	private static int prevmin;
+
+	private static int count;
+
+	private static int LevelTec;
+
+	public PacmanEnemyOrange(int posEnemyI1, int posEnemyJ1) {
+		posEnemyI = posEnemyI1;
+		posEnemyJ = posEnemyJ1;
+
+		imin = 0;
+		mass = new int[5];
+		prevmin = -1;
+		count = 0;
+		LevelTec = 0;
+	}
+
+	// враги пакмена
+
+	public PacmanEnemyOrange() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void moveToLeftEnemy(int lastJ) {
+		if (posEnemyJ >= 1
+				|| (posEnemyJ == 1 && posEnemyI == 10 && (LevelTec == 2 || LevelTec == 3))) { // граница
+																								// пол€
+			posEnemyJ--;
+		} else if (posEnemyJ == 0) {
+			posEnemyJ = lastJ;
+		}
+
+	}
+
+	public static void moveToRightEnemy(int lastJ) {
+		if (posEnemyJ < lastJ
+				|| (posEnemyJ == 27 && posEnemyI == 10 && (LevelTec == 2 || LevelTec == 3))) { // граница
+																								// пол€
+			posEnemyJ++;
+		} else if (posEnemyJ == 28) {
+			posEnemyJ = 0;
+		}
+
+	}
+
+	public static void moveDownEnemy(int lastI) {
+		if (posEnemyI < lastI) { // граница пол€
+			posEnemyI++;
+		}
+
+	}
+
+	public static void moveUpEnemy() {
+		if (posEnemyI > 1) { // граница пол€
+			posEnemyI--;
+		}
+	}
+
+	// ......”ћЌџ… ѕ–»«–ј ...................06/04/20
+	// провер€ем клетки вокруг текущей и считаем рассто€ние от них до пакмена
+	public static void checkPosition(int array[][]) {
+		if (array[posEnemyI][posEnemyJ - 1] != 1) { // лева€
+			mass[0] = (int) Math.sqrt((posEnemyI - Pacman.PosHeroI)
+					* (posEnemyI - Pacman.PosHeroI)
+					+ (posEnemyJ - 1 - Pacman.PosHeroJ)
+					* (posEnemyJ - 1 - Pacman.PosHeroJ));
+			count++;
+		} else
+			mass[0] = -1;
+		if (array[posEnemyI + 1][posEnemyJ] != 1) { // нижн€€
+			mass[1] = (int) Math.sqrt((posEnemyI + 1 - Pacman.PosHeroI)
+					* (posEnemyI + 1 - Pacman.PosHeroI)
+					+ (posEnemyJ - Pacman.PosHeroJ)
+					* (posEnemyJ - Pacman.PosHeroJ));
+			count++;
+		} else
+			mass[1] = -1;
+		if (array[posEnemyI][posEnemyJ + 1] != 1) { // права€
+			mass[2] = (int) Math.sqrt((posEnemyI - Pacman.PosHeroI)
+					* (posEnemyI - Pacman.PosHeroI)
+					+ (posEnemyJ + 1 - Pacman.PosHeroJ)
+					* (posEnemyJ + 1 - Pacman.PosHeroJ));
+			count++;
+		} else
+			mass[2] = -1;
+		if (array[posEnemyI - 1][posEnemyJ] != 1) { // верхн€€
+			mass[3] = (int) Math.sqrt((posEnemyI - 1 - Pacman.PosHeroI)
+					* (posEnemyI - 1 - Pacman.PosHeroI)
+					+ (posEnemyJ - Pacman.PosHeroJ)
+					* (posEnemyJ - Pacman.PosHeroJ));
+			count++;
+		} else
+			mass[3] = -1;
+
+	}
+
+	public static void minRoad() {
+		int min = 10000;
+		for (int i = 0; i < 4; i++) {
+			if (mass[i] <= min && mass[i] != -1 && i != prevmin) {
+				min = mass[i];
+				imin = i;
+			}
+		}
+		if (imin == 0) {
+			prevmin = 2;
+		}
+		if (imin == 1) {
+			prevmin = 3;
+		}
+		if (imin == 3) {
+			prevmin = 1;
+		}
+		if (imin == 2) {
+			prevmin = 0;
+		}
+	}
+
+	public static void runClever(int array[][], int lastI, int lastJ, int Level) {
+		while (1 > 0) {
+			if (imin == 0) {
+				if (posEnemyJ == 0 || array[posEnemyI][posEnemyJ - 1] != 1) {
+					if (posEnemyI != 10 && posEnemyJ != 1)
+						checkPosition(array);
+					moveToLeftEnemy(lastJ);
+					break;
+				} else
+					imin++;
+
+			}
+			if (imin == 1) {
+				if (array[posEnemyI + 1][posEnemyJ] != 1) {
+					checkPosition(array);
+					moveDownEnemy(lastI);
+					break;
+				} else
+					imin++;
+			}
+			if (imin == 2) {
+				if (posEnemyJ == 28 || array[posEnemyI][posEnemyJ + 1] != 1) {
+					if (posEnemyI != 10 && posEnemyJ != 27)
+						checkPosition(array);
+					moveToRightEnemy(lastJ);
+					break;
+				} else
+					imin++;
+			}
+			if (imin == 3) {
+				if (array[posEnemyI - 1][posEnemyJ] != 1) {
+					checkPosition(array);
+					moveUpEnemy();
+					break;
+				} else
+					imin = 0;
+			}
+
+		}
+	}
+
+	public static void checkHero() {
+		if (posEnemyI == Pacman.PosHeroI && posEnemyJ == Pacman.PosHeroJ
+				&& PacmanField.energizer != true) {
+			PacmanField.setGameIsOver(true);
+		}
+		if (posEnemyI == Pacman.PosHeroI && posEnemyJ == Pacman.PosHeroJ
+				&& PacmanField.energizer == true) {
+			PacmanField.countPoint += 200;
+		}
+	}
+
+	// когда каунт мньше равен 2 то ходим рандомно ( по последнему мнимуму) если
+	// больше то выбираем куда идти
+	// преследователь-призрак
+	public void CleverEnemyPursuit(int array[][], int Level, int lastI,
+			int lastJ) {
+		LevelTec = Level;
+		if (posEnemyJ != 0 && posEnemyJ != 28) {
+			checkPosition(array);
+			if (count > 2) {
+				minRoad();
+				count = 0;
+			}
+		}
+		runClever(array, lastI, lastJ, Level);
+		checkHero();
+
+	}
+
+	// испуганный
+	public void afraidEnemy(int array[][], int lastI, int lastJ) {
+		if (posEnemyJ == 28 || array[posEnemyI][posEnemyJ + 1] != 1
+				&& posEnemyI != Pacman.PosHeroI
+				&& posEnemyJ + 1 != Pacman.PosHeroJ) {
+			moveToRightEnemy(lastJ);
+
+		} else if (array[posEnemyI - 1][posEnemyJ] != 1
+				&& posEnemyI - 1 != Pacman.PosHeroI
+				&& posEnemyJ != Pacman.PosHeroJ) {
+			moveUpEnemy();
+
+		} else if (posEnemyJ == 0 || array[posEnemyI][posEnemyJ - 1] != 1
+				&& posEnemyI != Pacman.PosHeroI
+				&& posEnemyJ - 1 != Pacman.PosHeroJ) {
+			moveToLeftEnemy(lastJ);
+		} else if (array[posEnemyI + 1][posEnemyJ] != 1
+				&& posEnemyI + 1 != Pacman.PosHeroI
+				&& posEnemyJ != Pacman.PosHeroJ) {
+			moveDownEnemy(lastI);
+
+		}
+	}
+
+	public void drawEnemy(Graphics g, int x, int y) {
+		int size = 2;
+		Polygon j1 = new Polygon();
+		j1.addPoint(posEnemyJ * y + 4 * size + size / 2, posEnemyI * x + size);
+		j1.addPoint(posEnemyJ * y + 5 * size + size / 2, posEnemyI * x + size
+				/ 4);
+		j1.addPoint(posEnemyJ * y + 6 * size + size / 2, posEnemyI * x + size
+				/ 4);
+		j1.addPoint(posEnemyJ * y + 8 * size + size / 2, posEnemyI * x + size);
+		j1.addPoint(posEnemyJ * y + 10 * size + size / 2, posEnemyI * x + size);
+		j1.addPoint(posEnemyJ * y + 11 * size, posEnemyI * x + 5 * size + size
+				/ 2);
+		j1.addPoint(posEnemyJ * y + 11 * size + size / 2, posEnemyI * x + 8
+				* size + size / 2);
+		j1.addPoint(posEnemyJ * y + size * 11, posEnemyI * x + 12 * size); //
+		j1.addPoint(posEnemyJ * y + 8 * size, posEnemyI * x + 8 * size);
+		j1.addPoint(posEnemyJ * y + 6 * size + size / 2, posEnemyI * x + size
+				* 12);
+		j1.addPoint(posEnemyJ * y + 5 * size, posEnemyI * x + 8 * size);
+		j1.addPoint(posEnemyJ * y + size + size / 2, posEnemyI * x + 12 * size);//
+		j1.addPoint(posEnemyJ * y + size, posEnemyI * x + 8 * size + size / 2);
+		j1.addPoint(posEnemyJ * y + size + size / 2, posEnemyI * x + 5 * size
+				+ size / 2);
+		j1.addPoint(posEnemyJ * y + size * 2, posEnemyI * x + size);
+		if (PacmanField.afraid()) {
+			g.setColor(Color.GRAY);
+		} else
+			g.setColor(Color.ORANGE);
+
+		if (LevelTec == 0)
+			g.setColor(Color.ORANGE);
+		g.fillPolygon(j1);
+		g.drawPolygon(j1);
+		g.setColor(Color.WHITE);
+		g.fillOval(posEnemyJ * y + 4 * size, posEnemyI * x + size * 3 + size
+				/ 2, 4, 5);
+		g.fillOval(posEnemyJ * y + 7 * size, posEnemyI * x + size * 3 + size
+				/ 2, 4, 5);
+		g.setColor(Color.BLACK);
+		g.fillOval(posEnemyJ * y + 5 * size + size / 16, posEnemyI * x + size
+				* 3 + size / 2 + size / 16, 2, 3);
+		g.fillOval(posEnemyJ * y + 8 * size + size / 16, posEnemyI * x + size
+				* 3 + size / 2 + size / 16, 2, 3);
+
+	}
+}
